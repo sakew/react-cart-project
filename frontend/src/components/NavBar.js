ff29011001fe4f5b30c5aaa6bc2a5062c243da89
sakew <dorinsecu@gmail.com>
2021-03-14 21:40:06 +0200

    navbar responsive styling

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -14,10 +14,12 @@ function NavBar() {
             {/* links */}
             <ul className="navbar__links">
                 <li>
-                    <Link to="/cart">
+                    <Link to="/cart" className="cart__link">
                         <i className="fas fa-shopping-cart"></i>
-                    Cart
-                    <span className="cartlogo__badge">0</span>
+                        <span>Cart
+                            <span className="cartlogo__badge">0</span>
+                        </span>
+
 
                         {/*  */}
                     </Link>
